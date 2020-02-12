2b6d01dbf4f4c08b3ebcca6dea10179911fab3d3
Alifya53 <alifyahussain53@gmail.com>
2020-02-13 00:10:43 +0500

    variable program done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// fn main() {
+//     let mut x = 50; // mutable variable
+//     println!("the value of x is : {}", x);
+//     x = 60;
+//     println!("the second value of x is : {}", x);
+// }
+
+// shadowing is the process where we can assign the value to certain variable and change again its data type
+
 fn main() {
-    println!("Hello, world!");
+    let x = 50; 
+    println!("the value of x is : {}", x);
+    let x = 50.5;
+    println!("the second value of x is : {}", x);
 }
+
